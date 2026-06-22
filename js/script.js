166e00e2f8df9864994f4c100b2de38123a6a45c
Mikael Krug <desenvolvimento7@orbedobrasil.com.br>
2026-06-22 10:06:36 -0300

    Alterações gerais e mudanças oficiais.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
-console.log("Javascript carregado com sucesso!");
+const container = document.getElementById('carousel');
 
-let nome = "Mikael";
-console.log("Bem vindo" + nome);
+function scrollCarousel(direction) {
+  if (!container) return;
+
+  const scrollAmount = 430; 
+  
+  
+  const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+  
+  if (direction === 1 && container.scrollLeft >= maxScrollLeft - 5) {
+    container.scrollTo({
+      left: 0,
+      behavior: 'smooth'
+    });
+  } 
+
+  else if (direction === -1 && container.scrollLeft <= 5) {
+    container.scrollTo({
+      left: maxScrollLeft,
+      behavior: 'smooth'
+    });
+  } 
+
+  else {
+    container.scrollBy({
+      left: direction * scrollAmount,
+      behavior: 'smooth'
+    });
+  }
+}
